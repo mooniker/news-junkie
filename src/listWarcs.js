dbf3e43dccf53b3f25e3c8847f7a985c2dee5a65
James Moon <mooniker@gmail.com>
2020-11-02 21:42:52 -0500

    Fix typo in prefix template

--- src/listWarcs.js
+++ src/listWarcs.js
@@ -4,8 +4,8 @@ const { parseDateString } = require('./utils')
 const {
   COMMONCRAWL_BUCKET = 'commoncrawl',
   COMMONCRAWL_NEWS_PREFIX = 'crawl-data/CC-NEWS/',
-  WARC_FILE_PREFIX = 'CC-NEWS-',
-  ARCHIVE_BEGIN_YEAR = 2016
+  WARC_FILE_PREFIX = 'CC-NEWS-'
+  // ARCHIVE_BEGIN_YEAR = 2016
 } = process.env
 
 /**
@@ -26,14 +26,26 @@ function listWarcs (params = {}) {
 
   if (!date) {
     const today = new Date()
-    date = today.getFullYear().toString() + today.getMonth().toString().padStart(2, '0')
+    date =
+      today.getFullYear().toString() +
+      today
+        .getMonth()
+        .toString()
+        .padStart(2, '0')
   }
+
   if (typeof date === 'number') {
     date = date.toString()
   }
-  if (typeof date !== 'string' || date.length > '20201104'.length || date.length < 4) {
+
+  if (
+    typeof date !== 'string' ||
+    date.length > '20201104'.length ||
+    date.length < 4
+  ) {
     throw new Error('Only ISO date strings are supported')
   }
+
   if (date.length === 5 || date.length === 7) {
     throw new Error('Invalid ISO date string')
   }
@@ -43,7 +55,7 @@ function listWarcs (params = {}) {
   Prefix += year + '/'
 
   if (day) {
-    Prefix += month + '/' + WARC_FILE_PREFIX + year + month + year
+    Prefix += month + '/' + WARC_FILE_PREFIX + year + month + day
   } else if (month) {
     Prefix += month + '/'
   }
@@ -51,19 +63,23 @@ function listWarcs (params = {}) {
   const s3 = new AWS.S3()
 
   if (verbose) {
-    console.log({ Prefix, date, year, month, day })
+    console.log({ Bucket, Prefix, date, year, month, day })
   }
 
-  return s3
-    .makeUnauthenticatedRequest('listObjectsV2', {
-      Bucket,
-      Prefix
-    })
-    .promise()
-    // .then(console.log)
-    .then(({ Name, Contents, KeyCount }) =>
-      ({ warcFiles: Contents.map(({ Key }) => ({ Bucket: Name, Key })), date: { year, month, day }, totalFileCount: KeyCount })
-    )
+  return (
+    s3
+      .makeUnauthenticatedRequest('listObjectsV2', {
+        Bucket,
+        Prefix
+      })
+      .promise()
+      // .then(console.log)
+      .then(({ Name, Contents, KeyCount }) => ({
+        warcFiles: Contents.map(({ Key }) => ({ Bucket: Name, Key })),
+        date: { year, month, day },
+        totalFileCount: KeyCount
+      }))
+  )
 }
 
 module.exports = listWarcs
